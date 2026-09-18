cdf7cd06437f4574aa89a09246337eaa322be225
tlgeee1 <adelof231@gmail.com>
2026-09-19 00:20:16 +0300

    Add Firebase configuration and initialization

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUjccgx9UpaeKnqcX-one0eA785AFPe2k",
+  authDomain: "elhamayel-poet.firebaseapp.com",
+  projectId: "elhamayel-poet",
+  storageBucket: "elhamayel-poet.firebasestorage.app",
+  messagingSenderId: "580829125881",
+  appId: "1:580829125881:web:b3e678b2363b953517d628",
+  measurementId: "G-0JJPN2RW9F"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
